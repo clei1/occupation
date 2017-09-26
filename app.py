15fe49ca499552f068b43dc2cb08249b9bfe9b69
clei1 <clei1@stuy.edu>
2017-09-26 19:12:13 -0400

    table added

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+import csv
+import random
 
 my_app = Flask(__name__)
 
@@ -6,11 +8,29 @@ my_app = Flask(__name__)
 def root():
     return "Hi everybody!"
 
+def dictionary():
+    dic = {}
+    with open('occupations.csv') as csvfile:
+        rows = csv.reader(csvfile, delimiter = "\n")
+        for row in rows:
+            tempRow = row[0].rsplit(',',1)
+            if(tempRow[1] != "Percentage"):
+                dic[tempRow[0]] = float(tempRow[1])
+    return dic
+
+def occupationGenerator():
+    dic = dictionary()
+    randomNum = random.uniform(0, dic["Total"])
+    
+    for key in dic:
+        if(key != "Total"):
+            randomNum -= dic[key]
+            if(randomNum < 0):
+                return key
+
 @my_app.route('/occupations')
 def occupations():
-    return "foo!"
-
-
+    return render_template('template.html', title="Occupations Generator", descriptive_heading="Occupations Generator", collection = dictionary(), l = len(dictionary()), occupation = occupationGenerator())
 
 if __name__ == '__main__':
     my_app.debug = True;
