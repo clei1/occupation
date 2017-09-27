519fbbbe7d633e764309587b07a34018a28240d6
Masha Zorin <masha.zorin@gmail.com>
2017-09-26 20:50:08 -0400

    Added links for each occupation

--- app.py
+++ app.py
@@ -6,25 +6,25 @@ my_app = Flask(__name__)
 
 @my_app.route('/')
 def root():
-    return "Hi everybody!"
+    return 'Hi everybody!'
 
 def dictionary():
     dic = {}
     with open('occupations.csv') as csvfile:
         rows = csv.reader(csvfile, delimiter = "\n")
         for row in rows:
-            tempRow = row[0].rsplit(',',1)
+            tempRow = row[0].rsplit(',',2)
             if(tempRow[1] != "Percentage"):
-                dic[tempRow[0]] = float(tempRow[1])
+                dic[tempRow[0]] = (float(tempRow[1]), tempRow[2])
     return dic
 
 def occupationGenerator():
     dic = dictionary()
-    randomNum = random.uniform(0, dic["Total"])
+    randomNum = random.uniform(0, dic["Total"][0])
     
     for key in dic:
         if(key != "Total"):
-            randomNum -= dic[key]
+            randomNum -= dic[key][0]
             if(randomNum < 0):
                 return key
 
